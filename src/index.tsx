671162de37a344d9ed1100347dcce2d23cccd7eb
igoist <493512132@qq.com>
2018-07-13 11:58:57 +0800

    master: extract(split) code of menu, route

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+  BrowserRouter as Router
 } from 'react-router-dom';
 
-import AppHello from './usecase/AppHello';
-import AppModal from './usecase/AppModal';
-import AppMessage from './usecase/AppMessage';
-import AppUtil from './usecase/AppUtil';
-
-const currentUrl = '/~egoist/Apps/TS/dist/';
+import { Menu, Routes } from './router';
 
 const handleMenuBtnClick = () => {
   document.body.classList.toggle('nav-opened');
@@ -21,37 +14,13 @@ const handleMenuBtnClick = () => {
 ReactDOM.render(
   <Router>
     <div>
-      <ul className='siteMenu'>
-        <li>
-          <Link to={ currentUrl }>
-            <button>Home</button>
-          </Link>
-        </li>
-        <li>
-          <Link to={ currentUrl + 'message' }>
-            <button>Components: Message</button>
-          </Link>
-        </li>
-        <li>
-          <Link to={ currentUrl + 'util' }>
-            <button>Util: log</button>
-          </Link>
-        </li>
-        <li>
-          <Link to={ currentUrl + 'b' }>
-            <button>Hello: Rendering with React</button>
-          </Link>
-        </li>
-      </ul>
+      <Menu />
       <a className='menuBtn' onClick={ handleMenuBtnClick }>
         <del className='bar'></del>
         <del className='bar'></del>
         <del className='bar'></del>
       </a>
-      <Route exact path={ currentUrl } component={ AppModal } />
-      <Route path={ currentUrl + 'message' } component={ AppMessage } />
-      <Route path={ currentUrl + 'util' } component={ AppUtil } />
-      <Route path={ currentUrl + 'b' } component={ AppHello } />
+      <Routes />
     </div>
   </Router>,
   document.getElementById('app')
---
+++ src/router.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+
+import {
+  Route,
+  Link
+} from 'react-router-dom';
+
+import AppHello from './usecase/AppHello';
+import AppModal from './usecase/AppModal';
+import AppMessage from './usecase/AppMessage';
+import AppUtil from './usecase/AppUtil';
+
+const currentUrl = '/~egoist/Apps/TS/dist/';
+
+const routerArr = [
+  {
+    path: currentUrl,
+    title: 'Home',
+    component: AppModal
+  },
+  {
+    path: currentUrl + 'message',
+    title: 'Components: Message',
+    component: AppMessage
+  },
+  {
+    path: currentUrl + 'log',
+    title: 'Util: log',
+    component: AppUtil
+  },
+  {
+    path: currentUrl + 'b',
+    title: 'Hello: Rendering with React',
+    component: AppHello
+  }
+];
+
+export default class Menu extends React.Component {
+  render() {
+    let rows = [];
+
+    for (let i = 0; i < routerArr.length; i++) {
+      let item = routerArr[i];
+      rows.push(
+        <li key={ i.toString() }>
+          <Link to={ item.path }>
+            { item.title }
+          </Link>
+        </li>
+      );
+    }
+
+    return (
+      <>
+        <ul className='siteMenu'>
+          { rows }
+        </ul>
+      </>
+    );
+  }
+}
+
+class Routes extends React.Component {
+  render() {
+    let rows = [];
+
+    for (let i = 0; i < routerArr.length; i++) {
+      let item = routerArr[i];
+      if (item.path === currentUrl) {
+        rows.push(<Route key={ i.toString() } exact path={ item.path } component={ item.component } />);
+      } else {
+        rows.push(<Route key={ i.toString() } path={ item.path } component={ item.component } />);
+      }
+    }
+
+    return (
+      <>
+        { rows }
+      </>
+    );
+  }
+}
+
+export { Menu, Routes }
